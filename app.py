8c6f17b15263c618f414ca51e8280a3750c75c37
Risaal Khan <59867674+risaal-khan@users.noreply.github.com>
2024-12-23 19:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load models
-clf_model = joblib.load("Classification_randomforest.pkl")
-reg_model = joblib.load("optimized_xgb_model.pkl")
+clf_model = joblib.load("Weather-Forecasting/Classification_randomforest.pkl")
+reg_model = joblib.load("Weather-Forecasting/optimized_xgb_model.pkl")
 
 # Title and description
 st.title("🌦️ Weather Prediction App")
